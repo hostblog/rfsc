7d0cb1cba6d7b1022644c8f2861c4e3bdaa8ed3b
hostblog <39641129+hostblog@users.noreply.github.com>
2022-01-14 06:55:21 +0700

    Update rfsc.js

--- rfsc.js
+++ rfsc.js
@@ -1,6 +1,5 @@
 console.log('test1')
-if (window.self !== window.top) {
-  $( document ).ready(function() {
-    $('.leaflet-pane.leaflet-overlay-pane').remove()
-  }
-}
+	$(document).ready(function() {
+			$('.leaflet-pane.leaflet-overlay-pane').remove()
+		}
+	}
